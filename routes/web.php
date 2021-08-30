6a20409007f75145bfbc4588ca5c350938aef81c
Hamza-azouzi <hahi18025@gmail.com>
2021-08-30 15:59:08 +0100

    3

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,16 @@ use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/dashboard',[DashboardController::class , 'index'])->name('dashboard');
 
-Route::get('/register',[RegisterController::class , 'index'])->name('register');
 
+Route::get('/login',[LoginController::class , 'index'])->name('login');
+Route::post('/login',[LoginController::class , 'store']);
+
+
+Route::get('/register',[RegisterController::class , 'index'])->name('register');
 Route::post('/register',[RegisterController::class , 'store']);
 
-Route::get('/posts', function () {
 
-    return view('posts.index');
+Route::get('/posts', function () {
     
+    return view('posts.index');
 });
